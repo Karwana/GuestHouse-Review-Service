c3378009db8b60c0fd7b6aaa1cd212d5317b2b0f
Karwan Ali <karwana0000@gmail.com>
2026-08-30 14:07:59 +0200

    configure pom.xml and application.properties for review-service

--- src/main/java/org/nackademin/reviewservice/GuestHouseReviewServiceApplication.java
+++ src/main/java/org/nackademin/reviewservice/GuestHouseReviewServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuestHouseReviewServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuestHouseReviewServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GuestHouseReviewServiceApplication.class, args);
+    }
 
 }
