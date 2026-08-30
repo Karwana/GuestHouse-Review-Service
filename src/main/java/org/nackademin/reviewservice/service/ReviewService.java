9bc5857afdd28844900f5ae169e0bd6e674c3f91
Karwan Ali <karwana0000@gmail.com>
2026-08-30 14:27:57 +0200

    add ReviewService

---
+++ src/main/java/org/nackademin/reviewservice/service/ReviewService.java
@@ -0,0 +1,16 @@
+package org.nackademin.reviewservice.service;
+
+import org.nackademin.reviewservice.dto.ReviewDto;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    ReviewDto saveReview(ReviewDto reviewDto);
+
+    List<ReviewDto> getReviewsByRoom(Long roomId);
+
+    List<ReviewDto> getReviewsByCustomer(Long customerId);
+
+    void deleteReview(Long id);
+}
